538db8a9b6eefce9554d51096fb4e2a1e2abbdc5
execom <33149435+croexe@users.noreply.github.com>
2023-07-16 00:44:06 +0200

    Deserialization of HttpClient response

---
+++ JSON.Deserialize.Response.Consumer/Models/Temperature.cs
@@ -0,0 +1,13 @@
+namespace JSON.Deserialize.Response.Consumer.Models
+{
+    // JSON object copied to the clipboard, press Edit then Paste Special then Paste JSON as Code.
+
+    public class Temperature
+    {
+        public string date { get; set; }
+        public int temperatureC { get; set; }
+        public string summary { get; set; }
+        public int temperatureF { get; set; }
+    }
+
+}
---
+++ JSON.Deserialize.Response.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using JSON.Deserialize.Response.Consumer.Models;
+using System.Text.Json;
+
+var opt = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+using HttpClient client = new()
+{
+    BaseAddress = new Uri("http://localhost:5062")
+};
+
+var response = await client.GetAsync("/weatherforecast");
+
+if (response.IsSuccessStatusCode)
+{
+    var json = await JsonSerializer.DeserializeAsync<Temperature[]>(await response.Content.ReadAsStreamAsync(), opt);
+    if (json.Any())
+    {
+        foreach (var temp in json)
+        Console.WriteLine($"Summary: {temp.summary}");
+    }
+} else
+{
+    Console.WriteLine($"Something went wrong! Error: {response.StatusCode}");
+}
