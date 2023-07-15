4fd25762a450c5f5bf8bcb6e40093aa4acb94439
execom <33149435+croexe@users.noreply.github.com>
2023-07-15 14:40:34 +0200

    Json Serialization basics

---
+++ JSON/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace JSON.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public bool IsAlive { get; set; }
+    }
+}
---
+++ JSON/Program.cs
@@ -0,0 +1,23 @@
+﻿using JSON.Models;
+using System.Text.Json;
+
+var person = new Person
+{
+    Id = 1,
+    Age = 30,
+    FirstName = "John",
+    LastName = "McStevenson",
+    IsAlive = true,
+};
+
+var options = new JsonSerializerOptions {
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+}; 
+
+string json = JsonSerializer.Serialize<Person>(person, options);
+string jsonFile = "JohnMcStevenson.json";
+
+File.WriteAllText(jsonFile, json);
+
+Console.WriteLine(File.ReadAllText(jsonFile));
