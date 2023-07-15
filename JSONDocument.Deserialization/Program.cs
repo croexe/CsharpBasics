af41da2130d27346bf297a47fe96310d8fd6c8d5
execom <33149435+croexe@users.noreply.github.com>
2023-07-16 01:40:26 +0200

    Deserialization with JSONDocument

---
+++ JSONDocument.Deserialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+using HttpClient client = new()
+{
+    BaseAddress = new Uri("http://localhost:5062")
+};
+
+var response = await client.GetAsync("/weatherforecast");
+
+if (response.IsSuccessStatusCode)
+{
+    var jsonString = await response.Content.ReadAsStringAsync();
+    using(JsonDocument jsonDocument = JsonDocument.Parse(jsonString))
+    {
+        JsonElement root = jsonDocument.RootElement;
+        Console.WriteLine($"{root.ValueKind}, {root.ToString()}");
+        foreach(var temp in root.EnumerateArray())
+        {
+            Console.WriteLine($"Summary: {temp.GetProperty("summary")}");
+        }
+    }
+}
+else
+{
+    Console.WriteLine($"Something went wrong! Error: {response.StatusCode}");
+}
